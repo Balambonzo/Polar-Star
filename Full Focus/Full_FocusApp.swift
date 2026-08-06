5424e5a7996faddd7ee576c84dc0cd52c916c0c5
Alberto Toscano <albo.tosco@gmail.com>
2026-08-06 06:44:10 +0200

    Initial Commit

---
+++ Full Focus/Full_FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Full_FocusApp.swift
+//  Full Focus
+//
+//  Created by Alberto Toscano on 06/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct Full_FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
